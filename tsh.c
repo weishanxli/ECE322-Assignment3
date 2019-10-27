4bfdaa04e0c57f5989fc6a407c66020a330e17d9
Weishan <weishanxli@gmail.com>
2019-10-27 17:54:22 -0400

    foreground eval

--- tsh.c
+++ tsh.c
@@ -165,6 +165,35 @@ int main(int argc, char **argv)
 */
 void eval(char *cmdline) 
 {
+	char *argv[MAXARGS];
+	char buf[MAXLINE];
+	int bg;
+	pid_t pid;
+	
+	strcpy(buf, cmdline);
+	bg = parseline(buf, argv);
+	
+	if (argv[0] == NULL){
+		return;
+	}
+	
+	if (!builtin_command(argv)){
+		if (pid = Fork()) == 0){
+			if (execve(argv[0], argv, environ) < 0){
+				printf("%s: Command not found.\n", argv[0]);
+				exit(0
+			}
+		}
+		if (!bg) {
+			int status;
+			if (waitpid(pid, $status, 0) < 0) {
+				unix_error("waitfg: waitpid error");
+			}
+		}
+		else {
+			printf("%d %s", pid, cmdline);
+		}	
+	}
     return;
 }
 
